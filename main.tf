3adc2810f3d0b87871ca196d3449aa8efcaf2087
peacefulcheast <108151145+peacefulcheast@users.noreply.github.com>
2022-07-29 10:51:33 -0500

    added commented out potential fix

--- main.tf
+++ main.tf
@@ -19,3 +19,16 @@ module "eks-jx" {
   nginx_chart_version  = var.nginx_chart_version
   install_kuberhealthy = var.install_kuberhealthy
 }
+
+# module "kms_key" {
+#       source = "cloudposse/kms-key/aws"
+#       # Cloud Posse recommends pinning every module to a specific version
+#       # version = "x.x.x"
+#       namespace               = "eg"
+#       stage                   = "test"
+#       name                    = "chamber"
+#       description             = "KMS key for chamber"
+#       deletion_window_in_days = 10
+#       enable_key_rotation     = true
+#       alias                   = "alias/parameter_store_key"
+#     }
